6dc0786d4cdd260cbb14520194dc227f60a2e0e1
Mauricio González <hello@mauro-gonzalez.dev>
2024-10-28 12:30:10 -0300

    testing inline

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ export function app(): express.Express {
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-        inlineCriticalCss: false
+        inlineCriticalCss: true
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
